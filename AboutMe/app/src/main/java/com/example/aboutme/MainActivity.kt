424c3cf8c8db8eaf0ad833b69b3535b40699c98b
Jose Guevara <94gjose@gmail.com>
2022-04-29 18:21:49 -0400

    add nickname by text input

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    findViewById<Button>(R.id.button_done)?.setOnClickListener {
+      hideKeyboard()
+      addNickname()
+      it.visibility = Button.GONE
+    }
+
+  }
+
+  private fun hideKeyboard() {
+    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+  }
+
+  private fun addNickname() {
+    showNickname(getNickname())
+  }
+
+  private fun showNickname(aNickname: String) {
+    findViewById<TextView>(R.id.nickname_text).let {
+      it.text = "Hi!, my friends call me $aNickname"
+      it.visibility = TextView.VISIBLE
+    }
+  }
+
+  private fun getNickname(): String {
+    var result: String = ""
+    findViewById<EditText>(R.id.nickname_edit)?.let {
+      result = it.text.toString()
+      it.visibility = EditText.GONE
+    }
+    return result
   }
 }
