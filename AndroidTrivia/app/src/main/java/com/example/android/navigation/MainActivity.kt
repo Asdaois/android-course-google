f83b8bebc6b0a87daa18efcfa5536780f8b07c52
Jose Guevara <94gjose@gmail.com>
2022-05-02 14:12:16 -0400

    fix id of navigation controller

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,15 +19,23 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-    }
-    // TODO (02) Add the Navigation Graph to the Project
-    // Right click on the res directory and select New > Android resource file
-    // Select Navigation as the resource type and give it the file name navigation
+  private var navController: NavController? = null
+    get() = findNavController(R.id.navHostFragment)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    @Suppress("UNUSED_VARIABLE")
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    NavigationUI.setupActionBarWithNavController(this, navController!!)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    return navController!!.navigateUp()
+  }
 }
