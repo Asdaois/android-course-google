6cd4a6094a5dfd316b017f758699f812e206a9dd
Jose Guevara <94gjose@gmail.com>
2022-05-05 15:49:22 -0400

    refactor: clean warnings

--- GuessWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,10 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -42,7 +40,7 @@ class GameFragment : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
+  ): View {
 
     // Inflate view and obtain an instance of the binding class
     binding = DataBindingUtil.inflate<GameFragmentBinding>(
